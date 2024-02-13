bdf1b79fd2dfecfa82e6e364c20c1af9b33482d3
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2024-02-13 23:41:01 +0200

    Calclulator return NaN

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,74 @@
 "use strict";
-function printInConsole(...[]) {
-    for (let i = 0; i < arguments.length; i++) {
-        console.log(`The Value Is ${arguments[i]} And Type Is ${typeof arguments[i]}`);
+class Calculator {
+    constructor() {
+        this.num1 = null;
+        this.num2 = null;
+        this.sign = "";
     }
-    console.log("Done");
+    Add() {
+        if (this.num1 && this.num2)
+            return this.num1 + this.num2;
+        else
+            return "Error";
+    }
+    Sub() {
+        if (this.num1 && this.num2)
+            return this.num1 - this.num2;
+        else
+            return "Error";
+    }
+    Multiply() {
+        if (this.num1 && this.num2)
+            return this.num1 * this.num2;
+        else
+            return "Error";
+    }
+    Devide() {
+        if (this.num1 != null && this.num2 != null)
+            return this.num1 / this.num2;
+        else
+            return "Error";
+    }
+    GetSign(s) {
+        this.sign = s;
+    }
+    Clear() {
+        this.constructor();
+    }
 }
-console.log(printInConsole(1, 2, 3, 4, 5));
-console.log(printInConsole("A", "B", "C"));
-console.log(printInConsole(true, false, false, true, true));
+let calculator = new Calculator();
+let display = document.getElementById("display");
+function appendToDisplay(value) {
+    if (display) {
+        display.value += value;
+    }
+    if (value == "+" || value == "-" || value == "*" || value == "/") {
+        calculator.GetSign(value);
+    }
+    else {
+        if (calculator.num1 == null)
+            calculator.num1 = parseInt(value);
+        else
+            calculator.num2 = parseInt(value);
+    }
+}
+function clearDisplay() {
+    if (display) {
+        display.value = "";
+        calculator.Clear();
+    }
+}
+function calculateResult() {
+    appendToDisplay('=');
+    if (display) {
+        if (calculator.sign == "+")
+            display.value += calculator.Add();
+        else if (calculator.sign == "-")
+            display.value += calculator.Sub();
+        else if (calculator.sign == "*")
+            display.value += calculator.Multiply();
+        else if (calculator.sign == "/")
+            display.value += calculator.Devide();
+    }
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,81 @@
-function printInConsole(...[]:any) {
-  for (let i = 0; i < arguments.length; i++) {
-    console.log(`The Value Is ${arguments[i]} And Type Is ${typeof arguments[i]}`);
+class Calculator {
+  num1: number | null;
+  num2: number | null;
+  sign: string;
+
+  constructor() {
+    this.num1 = null;
+    this.num2 = null;
+    this.sign = "";
   }
-  console.log("Done");
+
+  Add(): number | string {
+    if (this.num1 && this.num2) return this.num1 + this.num2;
+    else return "Error";
+  }
+  Sub(): number | string {
+    if (this.num1 && this.num2) return this.num1 - this.num2;
+    else return "Error";
+  }
+  Multiply(): number | string {
+    if (this.num1 && this.num2) return this.num1 * this.num2;
+    else return "Error";
+  }
+  Devide(): number | string {
+    if (this.num1 != null && this.num2 != null) return this.num1 / this.num2;
+    else return "Error";
+  }
+
+  GetSign(s: string) {
+    this.sign = s;
+  }
+  Clear() {
+    this.constructor();
+  }
+}
+let calculator = new Calculator();
+// console.log(calculator.Add());
+// calculator.num1 = 8;
+// console.log(calculator.Add())
+let display: HTMLInputElement | null = document.getElementById(
+  "display"
+) as HTMLInputElement;
+
+function appendToDisplay(value: string) {
+  if (display) {
+    display.value += value;
+  }
+  if (value == "+" || value == "-" || value == "*" || value == "/") {
+    calculator.GetSign(value);
+  } else {
+    if (calculator.num1 == null) calculator.num1 = parseInt(value);
+    else calculator.num2 = parseInt(value);
+  }
 }
 
-console.log(printInConsole(1, 2, 3, 4, 5));
-console.log(printInConsole("A", "B", "C"));
-console.log(printInConsole(true, false, false, true, true));
+function clearDisplay() {
+  if (display) {
+    display.value = "";
+    calculator.Clear();
+  }
+}
+
+function calculateResult() {
+  // if (display) {
+  //   try {
+  //     display.value = eval(display.value);
+  //   } catch (error) {
+  //     display.value = "Error";
+  //   }
+  // }
+  appendToDisplay('=');
+  if (display) {
+    if (calculator.sign == "+") display.value += calculator.Add();
+    else if (calculator.sign == "-") display.value += calculator.Sub();
+    else if (calculator.sign == "*") display.value += calculator.Multiply();
+    else if (calculator.sign == "/") display.value += calculator.Devide();
+  }
+}
+
+// let rect = new Rectangle(5, 10);
+// console.log(rect.calculateArea()); // Output: 50
